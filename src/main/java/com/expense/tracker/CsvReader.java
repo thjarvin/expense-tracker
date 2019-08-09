5ce0ea5313b358fc6bd756c97e1078e091c1a7cd
Timo Järvinen <timo.jarvinen@yahoo.com>
2019-08-10 02:55:41 +0300

    Refactor replaced two for loops with Java stremas solutions.

--- src/main/java/com/expense/tracker/CsvReader.java
+++ src/main/java/com/expense/tracker/CsvReader.java
@@ -1,22 +1,20 @@
 package com.expense.tracker;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import org.springframework.context.annotation.Configuration;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class CsvReader {
 	private static final String CSV_DIR_NAME = "/home/timo/Asiakirjat/Test/csv";
 	
@@ -24,46 +22,47 @@ public class CsvReader {
 	}
 	
 	public List<Expense> getExpenses() throws Exception {
-		List<Expense> expenses = new ArrayList<>();
-		
-		for (Path path : getPaths(CSV_DIR_NAME)) {
-			expenses.addAll(expenseBeanBuilder(path, Expense.class));
-		}
-		
-	    return expenses;
+		return getPaths(CSV_DIR_NAME).
+				map(x -> expenseBeanBuilder(x, Expense.class)).
+				flatMap(x -> x.stream()).
+				collect(Collectors.toList());
 	}
 	
-	private List<Expense> expenseBeanBuilder(Path path, Class clazz) throws Exception {
+	private List<Expense> expenseBeanBuilder(Path path, Class clazz) {
 	     CsvTransfer csvTransfer = new CsvTransfer();
 	     ColumnPositionMappingStrategy ms = new ColumnPositionMappingStrategy();
 	     ms.setType(clazz);
+	     Reader reader = null;
+	     
+	     try {
+	    	 reader = Files.newBufferedReader(path);
+	    	 CsvToBean cb = new CsvToBeanBuilder(reader)
+	    			 .withType(clazz)
+	    			 .withMappingStrategy(ms)
+	    			 .withSeparator(';')
+	    			 .withSkipLines(1)
+	    			 .build();
 	 
-	     Reader reader = Files.newBufferedReader(path);
-	     CsvToBean cb = new CsvToBeanBuilder(reader)
-	       .withType(clazz)
-	       .withMappingStrategy(ms)
-	       .withSeparator(';')
-	       .withSkipLines(1)
-	       .build();
-	 
-	    csvTransfer.setCsvList(cb.parse());
-	    reader.close();
+	    	 csvTransfer.setCsvList(cb.parse());
+	    	 reader.close();
+	     } catch(Exception x) {
+	    	 throw new RuntimeException("Failed to read csv file.", x);
+	     } finally {
+	    	 try {
+				reader.close();
+			} catch (IOException x) {
+				throw new RuntimeException("Failed to close reader.", x);
+			}
+	     }
+	     
 	    return csvTransfer.getCsvList();
 	}
 	
-	private List<Path> getPaths(String dirName) {
-		List<Path> paths = new ArrayList<>();
+	private Stream<Path> getPaths(String dirName) {
 		File fileName = new File(dirName);
         File[] fileList = fileName.listFiles();
-        
-        for (File file: fileList) {
-        	if (file.isFile() && file.getAbsolutePath().endsWith(".csv")) {
-        		paths.add(Paths.get(file.getAbsolutePath()));
-        		log.info("Added path '" + file.getAbsolutePath() + "'.");
-        	} else {
-        		log.warn("Path '" + file.getAbsolutePath() + "' not added.");
-        	}
-        }
-        return paths;
+        return Arrays.stream(fileList).
+        		filter(x -> x.getAbsolutePath().endsWith(".csv")).
+        		map(x -> Paths.get(x.getAbsolutePath()));
 	}
 }
